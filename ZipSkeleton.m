a2e85bc97079b3e7ff389d527f0fc8d42cd90c17
System Administrator <root@ipsec-143-215-20-61.vpn.gatech.edu>
2014-04-09 23:21:45 -0400

    Exclude files in zip

--- ZipSkeleton.m
+++ ZipSkeleton.m
@@ -1,6 +1,7 @@
 fn = dir('.');
 
-exc = {'_site','.git','*~','.','..','.gitignore'};
+% exclude files in zip
+exc = {'_site','.git','*~','.','..','.gitignore','ZipSkeleton.m~','ZipSkeleton.m'};
 
 zip('jekyll-skeleton.zip',{fn( find(~ismember( {fn.name}, exc ))).name});
 
